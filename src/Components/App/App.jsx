1f3c25cd8f3ea22815e91782cca7df6eff21f0b6
Atharva Pardeshi <atharva.exe@gmail.com>
2022-10-17 11:12:42 +0530

    feat: import and render SearchResults

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
 
 import "./App.css";
 
@@ -14,7 +15,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            {/* <SearchResults /> */}
+            <SearchResults />
             {/* <Playlist /> */}
           </div>
         </div>
