d8e29c735d89d239d26c529295ef6ac938f645ec
Atharva Pardeshi <atharva.exe@gmail.com>
2022-10-17 11:01:09 +0530

    feat: import and render SearchBar, separate imports so they're easily readable

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+
+import SearchBar from "../SearchBar/SearchBar";
+
 import "./App.css";
 
 class App extends React.Component {
@@ -9,7 +12,7 @@ class App extends React.Component {
           Ja<span className="highlight">mmm</span>ing
         </h1>
         <div className="App">
-          {/* <SearchBar /> */}
+          <SearchBar />
           <div className="App-playlist">
             {/* <SearchResults /> */}
             {/* <Playlist /> */}
