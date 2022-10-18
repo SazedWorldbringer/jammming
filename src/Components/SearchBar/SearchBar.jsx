8bbbfa31d66bb6c79d8b18a4e3d6765b40f5bb99
Atharva Pardeshi <atharva.exe@gmail.com>
2022-10-18 20:10:58 +0530

    feat: add onChange attribute to input with value handleTermChange

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -20,7 +20,10 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
+        <input
+          onChange={this.handleTermChange}
+          placeholder="Enter A Song, Album, or Artist"
+        />
         <button className="SearchButton">SEARCH</button>
       </div>
     );
