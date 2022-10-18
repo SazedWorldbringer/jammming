bddea0c1a497a2e6bbbe04a9f6e810d2770c4fb7
Atharva Pardeshi <atharva.exe@gmail.com>
2022-10-18 18:39:26 +0530

    feat: make a new search method that uses props.onSearch to log out search term, initialize state of the search term to an empty string, write handleTermChange method that sets search term state to value of the event

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -2,6 +2,21 @@ import React from "react";
 import "./SearchBar.css";
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: "",
+    };
+  }
+
+  search = () => {
+    this.props.onSearch(this.state.term);
+  };
+
+  handleTermChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
   render() {
     return (
       <div className="SearchBar">
