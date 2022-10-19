068ba23bb0099e63e8213d73df13b71fd8a70138
Atharva Pardeshi <atharva.exe@gmail.com>
2022-10-19 13:28:15 +0530

    feat: import Spotify module. update search method by Spotify.search, set state of searchResults with search methods response

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -4,6 +4,8 @@ import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 
+import Spotify from "../../util/Spotify";
+
 import "./App.css";
 
 class App extends React.Component {
@@ -79,7 +81,9 @@ class App extends React.Component {
   };
 
   search = (term) => {
-    console.log(term);
+    Spotify.search(term).then((searchResults) => {
+      this.setState({ searchResults: searchResults });
+    });
   };
 
   render() {
